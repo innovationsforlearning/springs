cd6d9294e7220af616592616e4255aa8861312e1
Johnafriedman <john@innovationsforlearning.org>
2015-03-03 15:36:04 -0600

    add audio and background

--- www/js/tween.js
+++ www/js/tween.js
@@ -14,6 +14,9 @@ IFL.Tween.GameState = function(game) {
 IFL.Tween.GameState.prototype.preload = function () {
   "use strict";
   this.game.load.image('dust', 'assets/dust_puff_0018.png');
+  this.game.load.image('moon', 'assets/Earth-from-the-moon_1024x768.jpg');
+  
+  this.game.load.audio('boom', 'assets/audio/boom.mp3');
 
 }
 
@@ -26,6 +29,8 @@ IFL.Tween.GameState.prototype.create = function() {
 
     this.WAIT_FOR_ONSET = 0;
     this.WAIT_FOR_RIME = 1;
+    this.WAIT_FOR_JOIN = 2;
+
     this.state = this.WAIT_FOR_ONSET;
 
     this.y1 = 500;
@@ -35,27 +40,29 @@ IFL.Tween.GameState.prototype.create = function() {
 
     // Sets background color to white.
     this.game.stage.backgroundColor = '#000000';
+    this.game.add.image(0, 0, 'moon');
 
+    this.fx = game.add.audio('boom');
 
     var style = { font: "165px DidactGothic", fill: "#ff0044", align: "center" };
-    this.onset = this.game.add.text(ox, y0, "bl", style);
+    this.onset = this.game.add.text(ox, y0, "m", style);
     this.onset.anchor.set(1.0,0.5);
-    var rime = this.game.add.text(rx, y0, "ank", style);
-    rime.anchor.set(0.0,0.5);
+    this.rime = this.game.add.text(rx, y0, "oon", style);
+    this.rime.anchor.set(0.0,0.5);
 
 
     this.oEmitter = game.add.emitter(ox, this.y1, 100);
     this.oEmitter.makeParticles('dust');
-    this.oEmitter.gravity = 200;
+    this.oEmitter.gravity = 100;
 
     this.rEmitter = game.add.emitter(rx, this.y1, 100);
-    //emitter.makeParticles('dust');
-    this.rEmitter.gravity = 200;
+    this.rEmitter.makeParticles('dust');
+    this.rEmitter.gravity = 100;
 
     oTween0 = this.game.add.tween(this.onset).to( { y: this.y1 }, 2000, Phaser.Easing.Bounce.Out, true);
-    rTween0 = this.game.add.tween(rime).to( { y: this.y1 }, 2000, Phaser.Easing.Bounce.Out);
+    rTween0 = this.game.add.tween(this.rime).to( { y: this.y1 }, 2000, Phaser.Easing.Bounce.Out);
     oTween1 = this.game.add.tween(this.onset).to( {x: this.game.world.centerX}, 1000, Phaser.Easing.Bounce.Out);
-    rTween1 = this.game.add.tween(rime).to( {x: this.game.world.centerX}, 1000, Phaser.Easing.Bounce.Out);
+    rTween1 = this.game.add.tween(this.rime).to( {x: this.game.world.centerX}, 1000, Phaser.Easing.Bounce.Out);
     oTween0.onComplete.add(function(){rTween0.start();}, this);
     rTween0.onComplete.add(function(){oTween1.start();rTween1.start()},this);
 
@@ -63,9 +70,26 @@ IFL.Tween.GameState.prototype.create = function() {
 
 IFL.Tween.GameState.prototype.update = function() {
     "use strict";
-    if((this.onset.y >= this.y1-10)&&(this.state == this.WAIT_FOR_ONSET)){
-      this.state = this.WAIT_FOR_RIME;
-      this.oEmitter.start(true, 2000, null, 10);
+    switch(this.state)
+    {
+      case this.WAIT_FOR_ONSET:
+        if(this.onset.y >= this.y1-10){
+          this.state = this.WAIT_FOR_RIME;
+          this.oEmitter.x = this.onset.x;
+          this.oEmitter.width = this.onset.width;
+          this.oEmitter.start(true, 2000, null, 10);
+          this.fx.play();
+        }
+      break;
+      case this.WAIT_FOR_RIME:
+        if(this.rime.y >= this.y1-10){
+          this.state = this.WAIT_FOR_JOIN;
+          this.rEmitter.x = this.rime.x;
+          this.rEmitter.width = this.rime.width;
+          this.rEmitter.start(true, 2000, null, 10);
+          this.fx.play();
+        }
+      break;
     }
 }
 /*
